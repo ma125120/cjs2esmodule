3151d092b47558047c462e3cc9e27ca01e2c07f3
mapengda <pengda.ma@weimob.com>
2021-03-11 18:06:04 +0800

    feat: vite支持目录名

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { isCjsFile, transformFileBase } from "./utils/base"
 
-export function cjs2esmVitePlugin() {
+/**
+ * 
+ * @param sourceDir 监听的目录，默认是src 
+ * @returns 
+ */
+export function cjs2esmVitePlugin({ sourceDir = 'src' } = {}) {
   return {
     name: 'cjs2esmVitePlugin',
     transform(src, id) {
-      const ROOT = process.cwd().replace(/\\/g, '/') + '/src'
+      const ROOT = process.cwd().replace(/\\/g, '/') + `/${sourceDir}`
 
       if (/\.js$/g.test(id) && id.startsWith(ROOT) && isCjsFile(src)) {
         // const { code, map } = transformFileBase(src, id)
